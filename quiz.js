ddbdc7a079995becffc4abaf3a54f0f44045d0c4
meuhd <noirane.se@gmail.com>
2021-08-23 12:28:23 -0300

    first commit

---
+++ quiz.js
@@ -0,0 +1,302 @@
+//Question bank
+var questionBank= [
+    {
+        question :'Achei difícil me acalmar',
+        option : ['Não se aplicou de maneira alguma',
+        'Aplicou-se em algum grau ou por pouco tempo',
+        'Aplicou-se em um grau considerável ou por uma boa parte do tempo',
+        'Aplicou-se muito ou na maioria do tempo'],
+        answer : 'Africa'
+    },
+
+    {
+        question :'Senti minha boca seca ',
+        option : ['Não se aplicou de maneira alguma',
+        'Aplicou-se em algum grau ou por pouco tempo',
+        'Aplicou-se em um grau considerável ou por uma boa parte do tempo',
+        'Aplicou-se muito ou na maioria do tempo'],
+        answer : 'Africa'
+    },
+
+    {
+        question :'Não consegui vivenciar nenhum sentimento positivo ',
+        option : ['Não se aplicou de maneira alguma',
+        'Aplicou-se em algum grau ou por pouco tempo',
+        'Aplicou-se em um grau considerável ou por uma boa parte do tempo',
+        'Aplicou-se muito ou na maioria do tempo'],
+        answer : 'Africa'
+    },
+
+    {
+        question :'Tive dificuldade em respirar em alguns momentos  sem ter feito nenhum esforço físico  ',
+        option : ['Não se aplicou de maneira alguma',
+        'Aplicou-se em algum grau ou por pouco tempo',
+        'Aplicou-se em um grau considerável ou por uma boa parte do tempo',
+        'Aplicou-se muito ou na maioria do tempo'],
+        answer : 'Africa'
+    },
+
+    {
+        question :'Achei difícil ter iniciativa para fazer as coisas',
+        option : ['Não se aplicou de maneira alguma',
+        'Aplicou-se em algum grau ou por pouco tempo',
+        'Aplicou-se em um grau considerável ou por uma boa parte do tempo',
+        'Aplicou-se muito ou na maioria do tempo'],
+        answer : 'Africa'
+    },
+
+    {
+        question :'Tive a tendência de reagir de forma exagerada às situações ',
+        option : ['Não se aplicou de maneira alguma',
+        'Aplicou-se em algum grau ou por pouco tempo',
+        'Aplicou-se em um grau considerável ou por uma boa parte do tempo',
+        'Aplicou-se muito ou na maioria do tempo'],
+        answer : 'Africa'
+    },
+
+    {
+        question :'Senti tremores (ex. nas mãos) ',
+        option : ['Não se aplicou de maneira alguma',
+        'Aplicou-se em algum grau ou por pouco tempo',
+        'Aplicou-se em um grau considerável ou por uma boa parte do tempo',
+        'Aplicou-se muito ou na maioria do tempo'],
+        answer : 'Africa'
+    },
+
+    {
+        question :'Senti que estava sempre nervoso ',
+        option : ['Não se aplicou de maneira alguma',
+        'Aplicou-se em algum grau ou por pouco tempo',
+        'Aplicou-se em um grau considerável ou por uma boa parte do tempo',
+        'Aplicou-se muito ou na maioria do tempo'],
+        answer : 'Africa'
+    },
+
+    {
+        question :'Preocupei-me com situações em que eu pudesse entrar em pânico e parecesse ridículo (a) ',
+        option : ['Não se aplicou de maneira alguma',
+        'Aplicou-se em algum grau ou por pouco tempo',
+        'Aplicou-se em um grau considerável ou por uma boa parte do tempo',
+        'Aplicou-se muito ou na maioria do tempo'],
+        answer : 'Africa'
+    },
+
+    {
+        question :'Senti que não tinha nada a desejar ',
+        option : ['Não se aplicou de maneira alguma',
+        'Aplicou-se em algum grau ou por pouco tempo',
+        'Aplicou-se em um grau considerável ou por uma boa parte do tempo',
+        'Aplicou-se muito ou na maioria do tempo'],
+        answer : 'Africa'
+    },
+
+    {
+        question :'Senti-me agitado',
+        option : ['Não se aplicou de maneira alguma',
+        'Aplicou-se em algum grau ou por pouco tempo',
+        'Aplicou-se em um grau considerável ou por uma boa parte do tempo',
+        'Aplicou-se muito ou na maioria do tempo'],
+        answer : 'Africa'
+    },
+
+    {
+        question :'Achei difícil relaxar',
+        option : ['Não se aplicou de maneira alguma',
+        'Aplicou-se em algum grau ou por pouco tempo',
+        'Aplicou-se em um grau considerável ou por uma boa parte do tempo',
+        'Aplicou-se muito ou na maioria do tempo'],
+        answer : 'Africa'
+    },
+
+    {
+        question :'Senti-me depressivo (a) e sem ânimo ',
+        option : ['Não se aplicou de maneira alguma',
+        'Aplicou-se em algum grau ou por pouco tempo',
+        'Aplicou-se em um grau considerável ou por uma boa parte do tempo',
+        'Aplicou-se muito ou na maioria do tempo'],
+        answer : 'Africa'
+    },
+
+    {
+        question :'Fui intolerante com as coisas que me impediam de continuar o que eu estava fazendo  ',
+        option : ['Não se aplicou de maneira alguma',
+        'Aplicou-se em algum grau ou por pouco tempo',
+        'Aplicou-se em um grau considerável ou por uma boa parte do tempo',
+        'Aplicou-se muito ou na maioria do tempo'],
+        answer : 'Africa'
+    },
+
+    {
+        question :'Senti que ia entrar em pânico  ',
+        option : ['Não se aplicou de maneira alguma',
+        'Aplicou-se em algum grau ou por pouco tempo',
+        'Aplicou-se em um grau considerável ou por uma boa parte do tempo',
+        'Aplicou-se muito ou na maioria do tempo'],
+        answer : 'Africa'
+    },
+
+    {
+        question :'Não consegui me entusiasmar com nada ',
+        option : ['Não se aplicou de maneira alguma',
+        'Aplicou-se em algum grau ou por pouco tempo',
+        'Aplicou-se em um grau considerável ou por uma boa parte do tempo',
+        'Aplicou-se muito ou na maioria do tempo'],
+        answer : 'Africa'
+    },
+
+
+    {
+        question :'Senti que não tinha valor como pessoa ',
+        option : ['Não se aplicou de maneira alguma',
+        'Aplicou-se em algum grau ou por pouco tempo',
+        'Aplicou-se em um grau considerável ou por uma boa parte do tempo',
+        'Aplicou-se muito ou na maioria do tempo'],
+        answer : 'Africa'
+    },
+
+ 
+
+    {
+        question :'Senti que estava um pouco emotivo/sensível demais ',
+        option : ['Não se aplicou de maneira alguma',
+        'Aplicou-se em algum grau ou por pouco tempo',
+        'Aplicou-se em um grau considerável ou por uma boa parte do tempo',
+        'Aplicou-se muito ou na maioria do tempo'],
+        answer : 'Africa'
+    },
+
+    {
+        question :'Sabia que meu coração estava alterado mesmo não tendo feito nenhum esforço físico (ex. aumento da frequência cardíaca, disritmia cardíaca) ',
+        option : ['Não se aplicou de maneira alguma',
+        'Aplicou-se em algum grau ou por pouco tempo',
+        'Aplicou-se em um grau considerável ou por uma boa parte do tempo',
+        'Aplicou-se muito ou na maioria do tempo'],
+        answer : 'Africa'
+    },
+
+    {
+        question :'Senti medo sem motivo ',
+        option : ['Não se aplicou de maneira alguma',
+        'Aplicou-se em algum grau ou por pouco tempo',
+        'Aplicou-se em um grau considerável ou por uma boa parte do tempo',
+        'Aplicou-se muito ou na maioria do tempo'],
+        answer : 'Africa'
+    },
+
+    {
+        question :'Senti que a vida não tinha sentido ',
+        option : ['Não se aplicou de maneira alguma',
+        'Aplicou-se em algum grau ou por pouco tempo',
+        'Aplicou-se em um grau considerável ou por uma boa parte do tempo',
+        'Aplicou-se muito ou na maioria do tempo'],
+        answer : 'Africa'
+    }
+]
+
+var question= document.getElementById('question');
+var quizContainer= document.getElementById('quiz-container');
+var scorecard= document.getElementById('scorecard');
+var option0= document.getElementById('option0');
+var option1= document.getElementById('option1');
+var option2= document.getElementById('option2');
+var option3= document.getElementById('option3');
+var next= document.querySelector('.next');
+var Anterior= document.querySelector('.Anterior');
+var points= document.getElementById('score');
+var span= document.querySelectorAll('span');
+var i=0;
+var score= 0;
+
+//function to display questions
+function displayQuestion(){
+    for(var a=0;a<span.length;a++){
+        span[a].style.background='none';
+    }
+    question.innerHTML= 'Questão'+(i+1)+' - '+questionBank[i].question;
+    option0.innerHTML= questionBank[i].option[0];
+    option1.innerHTML= questionBank[i].option[1];
+    option2.innerHTML= questionBank[i].option[2];
+    option3.innerHTML= questionBank[i].option[3];
+    stat.innerHTML= "Questão"+' '+(i+1)+' '+'de'+' '+questionBank.length;
+}
+
+//function to calculate scores
+function calcScore(e){
+    if(e.innerHTML===questionBank[i].answer && score<questionBank.length)
+    {
+        score= score+1;
+        document.getElementById(e.id).style.background= 'limegreen';
+    }
+    else{
+        document.getElementById(e.id).style.color= "pink";
+    }
+    setTimeout(nextQuestion,300);
+
+
+    // fazer uma função so para isso 
+
+
+}
+
+
+
+
+
+
+//function to display next question
+function nextQuestion(){
+    if(i<questionBank.length-1)
+    {   
+        // alert (i); 
+        i=i+1;
+        displayQuestion();
+    }
+    else{
+        points.innerHTML= score+ '/'+ questionBank.length;
+        quizContainer.style.display= 'none';
+        scoreboard.style.display= 'block'
+    }
+}
+
+
+//Questão anterior 
+function AnteriorQuestion(){
+    if(i>0)
+    {   
+       // alert (i); 
+        i=i-1;
+        displayQuestion();
+    }
+    else{
+       
+       
+    }
+}
+
+
+//click events to next button
+next.addEventListener('click',nextQuestion);
+Anterior.addEventListener('click', AnteriorQuestion);
+
+
+//Back to Quiz button event
+function backToQuiz(){
+    location.reload();
+}
+
+//function to check Answers
+function checkAnswer(){
+    var answerBank= document.getElementById('answerBank');
+    var answers= document.getElementById('answers');
+    answerBank.style.display= 'block';
+    scoreboard.style.display= 'none';
+    for(var a=0;a<questionBank.length;a++)
+    {
+        var list= document.createElement('li');
+        list.innerHTML= questionBank[a].answer;
+        answers.appendChild(list);
+    }
+}
+
+
+displayQuestion();
